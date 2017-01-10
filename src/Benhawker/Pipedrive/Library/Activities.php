7ecb0842640dfdfc82bfe783341a9a4fd8f2028d
plampson <plampson@wooxo.fr>
2017-01-10 09:57:32 +0100

    update getMany on activities

--- src/Benhawker/Pipedrive/Library/Activities.php
+++ src/Benhawker/Pipedrive/Library/Activities.php
@@ -4,18 +4,17 @@ use Benhawker\Pipedrive\Exceptions\PipedriveMissingFieldError;
 
 /**
  * Pipedrive Activities Methods
- *
  * Activities are appointments/tasks/events on a calendar that can be
  * associated with a Deal, a Person and an Organization. Activities can
  * be of different type (such as call, meeting, lunch or a custom type
  * - see ActivityTypes object) and can be assigned to a particular User.
  * Note that activities can also be created without a specific date/time.
- *
+
  */
-class Activities
-{
+class Activities {
     /**
      * Hold the pipedrive cURL session
+     *
      * @var Curl Object
      */
     protected $curl;
@@ -23,22 +22,22 @@ class Activities
     /**
      * Initialise the object load master class
      */
-    public function __construct(\Benhawker\Pipedrive\Pipedrive $master)
-    {
+    public function __construct(\Benhawker\Pipedrive\Pipedrive $master) {
         //associate curl class
         $this->curl = $master->curl();
     }
+
     public function getMany($start) {
-        return $this->curl->get("activities?start=$start&limit=100")['data'];
+        return $this->curl->get("activities?start=$start&limit=100&user_id=0")['data'];
     }
+
     /**
      * Adds a activity
      *
      * @param  array $data activity detials
      * @return array returns detials of the activity
      */
-    public function add(array $data)
-    {
+    public function add(array $data) {
 
         //if there is no subject or type set chuck error as both of the fields are required
         if (!isset($data['subject']) or !isset($data['type'])) {
@@ -47,6 +46,7 @@ class Activities
 
         return $this->curl->post('activities', $data);
     }
+
     /**
      * Update a activity
      *
