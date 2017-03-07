b191aaf6894ef4a27267e2b9ea70669c69fba6fc
plampson <plampson@wooxo.fr>
2017-03-07 17:11:38 +0100

    add webhooks management

--- src/Benhawker/Pipedrive/Library/Curl.php
+++ src/Benhawker/Pipedrive/Library/Curl.php
@@ -235,10 +235,10 @@ class Curl
                     // Fix "curl_setopt(): The usage of the @filename API for
                     // file uploading is deprecated. Please use the CURLFile
                     // class instead".
-                    elseif (is_string($value) && strpos($value, '@') === 0) {
-                        // add file
-                        $data[$key] = new CURLFile(substr($value, 1));
-                    }
+//                    elseif (is_string($value) && strpos($value, '@') === 0) {
+//                        // add file
+//                        $data[$key] = new CURLFile(substr($value, 1));
+//                    }
                 }
             }
         }
